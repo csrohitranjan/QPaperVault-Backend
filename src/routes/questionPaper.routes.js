4192aa2cfa9a7e41d9679c197f083b7204bec148
Rohit Ranjan <dev.rohitranjan@gmail.com>
2025-05-21 16:02:47 +0530

    Some minor issue Fixes

--- src/routes/questionPaper.routes.js
+++ src/routes/questionPaper.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { uploadQuestionPaper, getPendingQuestionPapers, getApprovedQuestionPapers, approveQuestionPaper } from "../controllers/questionPaper.controller.js";
 import { userAuth } from "../middlewares/userAuth.middleware.js";
+import { isAdminAuth } from "../middlewares/isAdminAuth.middleware.js";
 import uploadPDFonlocal from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
 router.post("/uploadQuestionPaper", userAuth, uploadPDFonlocal.single("questionPaper"), uploadQuestionPaper);
-router.get("/getPendingQuestionPapers", userAuth, getPendingQuestionPapers);
-router.put('/questionPaper/approve/:questionPaperId', approveQuestionPaper);
+router.get("/getPendingQuestionPapers", userAuth, isAdminAuth, getPendingQuestionPapers);
+router.put('/questionPaper/approve/:questionPaperId', userAuth, isAdminAuth, approveQuestionPaper);
 router.route("/getApprovedQuestionPapers").get(getApprovedQuestionPapers);
 
 
