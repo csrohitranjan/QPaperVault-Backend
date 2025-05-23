1f32c52e3d3c2df25c329f0aa8402abb84d610ce
Rohit Ranjan <dev.rohitranjan@gmail.com>
2025-05-23 13:35:37 +0530

    Mobile number added on user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema(
             unique: true,
             lowercase: true,
         },
+        mobileNumber: {
+            type: String,
+            // required: true,
+            unique: true,
+        },
         password: {
             type: String,
             required: true,
